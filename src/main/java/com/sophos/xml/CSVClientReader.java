d6ad8fcb8b85fae0a090a9a80b74766b3c241ef1
Jo3c5x3 <jonatan_aldana@gap.com>
2016-04-14 10:00:42 -0500

    Lector csv a prueba de errores

--- src/main/java/com/sophos/xml/CSVClientReader.java
+++ src/main/java/com/sophos/xml/CSVClientReader.java
@@ -11,28 +11,51 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
- *
- * @author jonatan
+ * Clase que permite leer el archivo CSV proporcionado por el cliente
+ * @author Rc
  */
 class CSVClientReader implements AutoCloseable {
     
     private CsvReader reader;
     private boolean canRead;
+    private boolean pendingRecord;
     
+    /**
+     * Constructor de la clase
+     * @param path
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
     CSVClientReader(String path) throws FileNotFoundException, IOException {
-        reader = new CsvReader(path);
+        reader = new CsvReader(path);  
         reader.readHeaders();
     }
     
+    /**
+     * Se identifica si hay registros por leer y retorna false si no hay.
+     * @return
+     * @throws IOException 
+     */
     boolean hasNext() throws IOException {
-        return canRead = reader.readRecord();
+        if (pendingRecord) return true;
+        else return pendingRecord = canRead = reader.readRecord();
     }
     
+    /**
+     * Se El objeto con los datos del CSV (cuatro columnas del archivo)
+     * @return
+     * @throws IOException 
+     */
     Client next() throws IOException {
         if (!canRead) throw new RuntimeException("No hay mas clientes por leer");
+        pendingRecord = false;
         return new Client(reader.get(0), reader.get(1), reader.get(2), reader.get(3));
     }
     
+    
+    /**
+     * Cierra la lectura de datos
+     */
     @Override public void close() {
         reader.close();
     }
